d956735efc59e76fd545e28498af937601a8e0f1
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-05-05 15:28:05 +0300

    fix our live demo

--- src/app.js
+++ src/app.js
@@ -23,9 +23,11 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true);
-      if (allowedOrigins.length === 0 || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
+
+      const isAllowed = allowedOrigins.some((o) => origin.startsWith(o));
+
+      if (isAllowed) return callback(null, true);
+
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
