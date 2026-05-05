5c2a506357f987241a7d398e4ea093002b47683f
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-05-05 10:46:45 +0300

    globalLimiter fixed

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(
 );
 app.use(helmet());
 app.use(requestLogger);
-app.use(globalLimiter);
+// app.use(globalLimiter);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(env.cookie.secret));
