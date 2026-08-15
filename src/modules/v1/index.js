40f692394fa8b153a9e4b859dd3eb36301696a7b
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-08-15 16:17:38 +0300

    Fixing some styles

--- src/modules/v1/index.js
+++ src/modules/v1/index.js
@@ -1,19 +1,20 @@
-import express from 'express';
+import express from "express";
 
-import authRouter from './auth/auth.routes.js';
-import userRouter from './users/users.routes.js';
-import leaveRouter from './leave/leave.routes.js';
-import attendanceRouter from './attendance/attendance.routes.js';
-import ticketRouter from './tickets/tickets.routes.js';
-import adminRouter from './admin.routes.js';
+import authRouter from "./auth/auth.routes.js";
+import userRouter from "./users/users.routes.js";
+import leaveRouter from "./leave/leave.routes.js";
+import attendanceRouter from "./attendance/attendance.routes.js";
+import ticketRouter from "./tickets/tickets.routes.js";
+import adminRouter from "./admin.routes.js";
 
 const router = express.Router();
 
-router.use('/auth', authRouter);
-router.use('/users', userRouter);
-router.use('/staff', attendanceRouter);
-router.use('/leaves', leaveRouter);
-router.use('/tickets', ticketRouter);
-router.use('/admin', adminRouter);
+router.use("/auth", authRouter);
+router.use("/users", userRouter);
+// router.use('/staff', attendanceRouter);
+router.use("/attendance", attendanceRouter);
+router.use("/leaves", leaveRouter);
+router.use("/tickets", ticketRouter);
+router.use("/admin", adminRouter);
 
 export default router;
